b55d4d1f94ced3247124ddd68441ad476c20c450
xixihhh321 <106596767+xixihhh321@users.noreply.github.com>
2022-07-16 17:10:48 +0800

    Update GitTest.java

--- src/main/java/com/sjtu/git/GitTest.java
+++ src/main/java/com/sjtu/git/GitTest.java
@@ -4,8 +4,10 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git");
         System.out.println("hello git1");
-        System.out.println("hello git2");
+        System.out.println("hello git2");commit
         System.out.println("我也修改了");
         System.out.println("hello git3");
+        System.out.println("pull commit");
+        
     }
 }
