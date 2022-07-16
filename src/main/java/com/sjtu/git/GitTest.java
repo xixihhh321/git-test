3c2a2509d728057157a49a253db06329dda7b17a
guiyu <352354534@qq.com>
2022-07-16 17:14:11 +0800

    fourth commit

--- src/main/java/com/sjtu/git/GitTest.java
+++ src/main/java/com/sjtu/git/GitTest.java
@@ -4,10 +4,9 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git");
         System.out.println("hello git1");
-        System.out.println("hello git2");commit
+        System.out.println("hello git2");
         System.out.println("我也修改了");
         System.out.println("hello git3");
         System.out.println("pull commit");
-        
     }
 }
